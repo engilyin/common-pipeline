226397115cc593c35e0066c7ab8009ccaf3dafec
Alex Ilin <engilyin@gmail.com>
2025-02-10 23:48:49 -0800

    full remove diag

--- vars/awsFullServiceRemove.groovy
+++ vars/awsFullServiceRemove.groovy
@@ -21,6 +21,7 @@ def call(Map vars) {
 
         echo "🔍 Finding the service..."
         def serviceArn = awsExistedServcieArn(clusterName: clusterName, servicePrefix: servicePrefix, serviceName: serviceName)
+        echo "Found service: ${serviceArn}"
 
         if(!serviceArn.isEmpty()) {
 
@@ -29,7 +30,10 @@ def call(Map vars) {
                 OLD_ENI_ID=\$(aws ecs describe-tasks --cluster ${clusterName} --tasks ${serviceArn} --query "tasks[0].attachments[0].details[?name=='networkInterfaceId'].value" --output text || true)
                 OLD_ENI_IP=\$(aws ec2 describe-network-interfaces --network-interface-ids \$OLD_ENI_ID --query "NetworkInterfaces[0].PrivateIpAddress" --output text || true)
 
+                echo "OLD_ENI_IP = \$OLD_ENI_IP"
+
                 if [[ ! -z "\$OLD_ENI_IP" ]]; then
+                    echo "Deregistering from target group"
                     aws elbv2 deregister-targets --target-group-arn ${targetGroupArn} --targets Id=\$OLD_ENI_IP,Port=8080
                 fi
 
