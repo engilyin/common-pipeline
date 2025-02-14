b7dafda69c991e32d6d6bb0229ea0159accaf541
Alex Ilin <engilyin@gmail.com>
2025-02-14 00:53:56 -0800

    Break up on the lines the list of services

--- vars/awsExistedServcieArn.groovy
+++ vars/awsExistedServcieArn.groovy
@@ -14,6 +14,6 @@ def call(Map vars) {
     def grepCommand = otherVersions ? "-v ${serviceName}" : "${serviceName}"
 
     return sh(script: """
-        aws ecs list-services --cluster ${clusterName} --query "serviceArns[?contains(@, '${servicePrefix}-')]" --output text | tr ' ' '\\n' | grep ${grepCommand} || true
+        aws ecs list-services --cluster ${clusterName} --query "serviceArns[?contains(@, '${servicePrefix}-')]" --output text | awk '{for (i=1; i<=NF; i++) print \$i}' | grep ${grepCommand} || true
     """,  returnStdout: true).trim()
 }
